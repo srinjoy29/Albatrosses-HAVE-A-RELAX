dd9ea57ea227985563f357d37fd456e2948ac697
Srinjoy Das <96923080+srinjoy29@users.noreply.github.com>
2024-04-26 07:32:36 +0530

    Update useAxiosSecure.jsx

--- frontend/src/hooks/useAxiosSecure.jsx
+++ frontend/src/hooks/useAxiosSecure.jsx
@@ -8,7 +8,7 @@ const useAxiosSecure = () => {
   const navigate = useNavigate();
 
   const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://albatrosses-have-a-relax.onrender.com/',
   });
 
   useEffect(() => {
